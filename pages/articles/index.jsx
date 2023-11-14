81c697494fb259111144c7e5e2185eba408a2e5a
kihiuFrank <franklinekihiu@gmail.com>
2023-11-14 11:02:05 +0300

    update

--- pages/articles/index.jsx
+++ pages/articles/index.jsx
@@ -1,16 +1,14 @@
-import { useState, useEffect } from "react";
-
 import Recent from "../../components/sections/articles/recent";
-
 import Color from "../../components/utils/page.colors.util";
-
 import colors from "../../content/articles/_colors.json";
 import settings from "../../content/_settings.json";
+import Spinner from "../../components/sections/spinner/Spinner";
 
 //
-export default function Articles({ mediumArticles }) {
+export default function Articles({ mediumArticles, isLoading }) {
   return (
     <>
+      {isLoading && <Spinner />}
       <Color colors={colors} />
       <Recent mediumArticles={mediumArticles} />
     </>
@@ -26,13 +24,29 @@ export async function getServerSideProps({ res }) {
 
   console.log(settings.username.medium);
 
-  const [mediumRSS] = await Promise.all([
-    fetch(
-      `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${settings.username.medium}`
-    ),
-  ]);
+  try {
+    const [mediumRSS] = await Promise.all([
+      fetch(
+        `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/${settings.username.medium}`
+      ),
+    ]);
 
-  let [mediumArticles] = await Promise.all([mediumRSS.json()]);
+    let [mediumArticles] = await Promise.all([mediumRSS.json()]);
 
-  return { props: { mediumArticles } };
+    return {
+      props: {
+        mediumArticles,
+        isLoading: false,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+
+    return {
+      props: {
+        mediumArticles: null,
+        isLoading: false,
+      },
+    };
+  }
 }
