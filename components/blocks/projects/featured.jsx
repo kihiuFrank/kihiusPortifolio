2df9f163e1304c83f9621317c710626401a3631b
kihiuFrank <franklinekihiu@gmail.com>
2025-02-21 02:24:17 +0300

    link update

--- components/blocks/projects/featured.jsx
+++ components/blocks/projects/featured.jsx
@@ -59,8 +59,7 @@ export default function FeaturedProject({ content }, index) {
       initial={["rest", "hidden"]}
       whileHover="hover"
       animate={controls}
-
-      //onClick={handleClick}
+      onClick={handleClick}
     >
       <div className={css.details}>
         <div className={css.projectHeader}>
